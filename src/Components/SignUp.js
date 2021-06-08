8f919a2bd577aad4b08d64b9d929cadd3ca0cd26
Adrian <adriann.kowalski@gmail.com>
2021-06-08 08:18:53 +0200

    signUp component styling

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
     return (
-        <div>
-            Hello and sign
+        <div id='SigUpContainer'>
+         <p>Please Sign Up</p>
+          <form id='SignUpPageForm'>
+            <input placeholder='Login' type='text' className='LoginInputs'></input>
+            <input placeholder='Password' type='password' className='LoginInputs'></input>
+            <button id='LoginPageButton' type='submit'>Sign Up</button>
+          </form>
+          <Link to="/">go back</Link>
         </div>
     )
 }
 
-export default SignUp
+export default SignUp;
